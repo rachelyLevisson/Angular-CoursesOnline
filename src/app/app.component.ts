88f43d86973fca4f42d086710dc2343526cd9c4f
לויסון <@DESKTOP-9G3ER3M>
2025-02-27 21:15:49 +0200

    fix good!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms'; // ייבוא FormsModule
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { ReactiveFormsModule } from '@angular/forms';
-import { LoginComponent } from './components/login/login.component';
-// import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule, provideAnimations } from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
-
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from "./components/register/register.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterModule,FormsModule,LoginComponent,
+  standalone: true,
+  imports: [
+    RouterModule,
+    FormsModule,
+    ReactiveFormsModule,
     CommonModule,
-    BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
     MatFormFieldModule,
-    ReactiveFormsModule],
-    providers: [provideAnimations()],
+    LoginComponent,
+    RegisterComponent
+],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'online-courses';
