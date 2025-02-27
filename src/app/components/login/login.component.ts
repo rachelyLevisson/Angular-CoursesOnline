76c9ea55ca31b73cd1a11f5461950c17c5386f08
לויסון <@DESKTOP-9G3ER3M>
2025-02-28 00:35:59 +0200

    server dont good

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,28 +2,30 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router, RouterOutlet } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule,RouterOutlet,CommonModule],
+  imports: [FormsModule, RouterOutlet, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
-  constructor(private router: Router) {} // הזרקת Router
-
   userName: string = '';
   password: string = '';
   flag: boolean = false;
+  user: any;
+
+  constructor(private router: Router, private authService: AuthService) {} // הזרקת Router
 
   send() {
     console.log(this.userName);
     console.log(this.password);
+    // this.user = this.authService.login({this.userName,this.password});
+
     this.router.navigate(['/courses']); // ניווט לקומפוננטה אחרת
-    console.log("i");
-    this.flag = true
-    console.log("flag: ", this.flag);
-    
+    console.log('i');
+    this.flag = true;
+    console.log('flag: ', this.flag);
   }
-  
 }
