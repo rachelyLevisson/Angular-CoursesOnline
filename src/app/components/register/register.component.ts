86c2f36b16dcec38e829dc54ab905759da157983
לויסון <@DESKTOP-9G3ER3M>
2025-03-10 00:55:38 +0200

    good night!!

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCardModule } from '@angular/material/card';
 import { AuthService } from '../../services/auth.service';
 import { Router, RouterOutlet } from '@angular/router';
-import { ReactiveFormsModule, FormGroup, FormControl, FormBuilder } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  FormBuilder,
+} from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-register',
@@ -18,39 +24,48 @@ import { MatInputModule } from '@angular/material/input';
     MatButtonModule,
     MatFormFieldModule,
     RouterOutlet,
-    MatCardModule
+    MatCardModule,
+    MatSelectModule,
   ],
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent {
   registrationForm: FormGroup;
-  selectedOption = new FormControl();
-  options = ['אפשרות 1', 'אפשרות 2', 'אפשרות 3'];
+  // selectedOption = new FormControl();
+  // options = ['student', 'techer', 'admin'];
+  protected readonly value = signal('');
+  items: any;
 
-  constructor(private fb: FormBuilder, private httpAuth : AuthService, private router : Router) {
+  constructor(
+    private fb: FormBuilder,
+    private httpAuth: AuthService,
+    private router: Router
+  ) {
     this.registrationForm = this.fb.group({
       name: [''],
       email: [''],
       password: [''],
-      role: ['']
+      role: [''],
     });
   }
 
+  protected onInput(event: Event) {
+    this.value.set((event.target as HTMLInputElement).value);
+  }
   onSubmit() {
     if (this.registrationForm.valid) {
       console.log('Form Submitted!', this.registrationForm.value);
-     this.httpAuth.register(this.fb).subscribe({
-      next: (res) => {
-        alert("הפרטים נשמרו בהצלחה!!");
-        this.router.navigate(['/courses'])
-      },
-      error: (e) => {
-        console.log(e);
-        alert("error!!! check this")
-      }
-     })
-      
+      this.httpAuth.register(this.fb).subscribe({
+        next: (res) => {
+          alert('הפרטים נשמרו בהצלחה!!');
+          this.router.navigate(['/courses']);
+        },
+        error: (e) => {
+          console.log(e);
+          alert('error!!! check this');
+        },
+      });
     }
   }
 }
